0e49c16f431811cf94adfdca8307213bd59887b2
Thalita Andrade <thalita.andrade21@gmail.com>
2025-09-23 22:05:29 -0300

    corrige workflow

--- provider.tf
+++ provider.tf
@@ -11,5 +11,5 @@ terraform {
 provider "google" {
   project     = var.project_id
   region      = var.region
-  credentials = jsondecode(base64decode(var.gcp_credentials))
+  credentials = var.gcp_credentials
 }
